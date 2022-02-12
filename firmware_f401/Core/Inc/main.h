d0f2416e142678b66e96766de8a8fed36ecf6033
mido0630 <hoge>
2022-02-13 02:31:56 +0900

    なんちゃって正弦波駆動

--- firmware_f401/Core/Inc/main.h
+++ firmware_f401/Core/Inc/main.h
@@ -31,7 +31,7 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "user_code/bldc_driver.h"
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
--- firmware_f401/Core/Src/main.c
+++ firmware_f401/Core/Src/main.c
@@ -24,11 +24,10 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-#include "math.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -39,6 +38,7 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define PWM_LIMIT 1000
+#define NUM_ANGLE_SPLIT 360
 #define U_H &htim1, TIM_CHANNEL_1
 /* USER CODE END PD */
 
@@ -52,6 +52,8 @@
 /* USER CODE BEGIN PV */
 static uint16_t adc_values[5];
 //uint16_t debug=0;
+ uint16_t dbg_arr[6];
+ uint16_t sin_arr[NUM_ANGLE_SPLIT];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -66,12 +68,15 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
     if (htim == &htim3){
 //    	5kHzTimer
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+//        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
     }
     if (htim == &htim11){
 //    	500HzTimer
 //        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
     }
+}
+void user_max_3items(float a,float b,float c){
+	;
 }
 /* USER CODE END 0 */
 
@@ -127,17 +132,31 @@ int main(void)
   HAL_ADC_Start_DMA(&hadc1,(uint32_t *)adc_values, 5);
   HAL_GPIO_WritePin (EN_GATE_GPIO_Port, EN_GATE_Pin, SET);
 
-  int counter=0;
+  int counter_U=0;
+  int counter_V=0;
+  int counter_W=0;
   int power=100;
-  int delay=10;
-
+  int delay=0;
+  uint16_t duty_U,duty_V,duty_W,duty_U_L,duty_V_L,duty_W_L;
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);//U_H
   __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);//U_L
   __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);//V_H
   __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);//V_L
   __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, 0);//W_H
   __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, 0);//W_L
+  duty_U_L=0;
+  duty_V_L=0;
+  duty_W_L=0;
 
+  //sin wave pre-calculation
+  HAL_GPIO_WritePin (LED_GPIO_Port, LED_Pin, SET);
+//  for(int i=0;i<NUM_ANGLE_SPLIT;i++)sin_arr[i]=sin(2*M_PI*i/NUM_ANGLE_SPLIT)*power;
+  HAL_GPIO_WritePin (LED_GPIO_Port, LED_Pin, RESET);
+  const int sin_arr[] = {128, 132, 136, 140, 143, 147, 151, 155, 159, 162, 166, 170, 174, 178, 181, 185, 189, 192, 196, 200, 203, 207, 211, 214, 218, 221, 225, 228, 232, 235, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 235, 232, 228, 225, 221, 218, 214, 211, 207, 203, 200, 196, 192, 189, 185, 181, 178, 174, 170, 166, 162, 159, 155, 151, 147, 143, 140, 136, 132, 128, 124, 120, 116, 113, 109, 105, 101, 97, 94, 90, 86, 82, 78, 75, 71, 67, 64, 60, 56, 53, 49, 45, 42, 38, 35, 31, 28, 24, 21, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 24, 28, 31, 35, 38, 42, 45, 49, 53, 56, 60, 64, 67, 71, 75, 78, 82, 86, 90, 94, 97, 101, 105, 109, 113, 116, 120, 124};
+
+  uint16_t phase_shift = NUM_ANGLE_SPLIT/ 3;
+  counter_V=counter_U+phase_shift;
+  counter_W=counter_V+phase_shift;
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -145,38 +164,38 @@ int main(void)
   while (1)
   {
 //	  debug=sin(2*M_PI*counter/1000)*PWM_LIMIT/2+PWM_LIMIT/2;
-	  //pahse1
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, power);
-	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, power);
+
+	  duty_U=sin_arr[counter_U];
+	  duty_V=sin_arr[counter_V];
+	  duty_W=sin_arr[counter_W];
+
+//	  if(duty_U==0)duty_U_L=PWM_LIMIT;
+//	  if(duty_V==0)duty_V_L=PWM_LIMIT;
+//	  if(duty_W==0)duty_W_L=PWM_LIMIT;
+
+	  dbg_arr[0]=duty_U;
+	  dbg_arr[1]=duty_V;
+	  dbg_arr[2]=duty_W;
+	  dbg_arr[3]=duty_U_L;
+	  dbg_arr[4]=duty_V_L;
+	  dbg_arr[5]=duty_W_L;
+
+
+	  //high_side
+	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, duty_U);
+	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, duty_V);
+	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, duty_W);
+//	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, duty_U);
+//	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, duty_V);
+//	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, duty_W);
+
 	  HAL_Delay(delay);
-	  //pahse2
-	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, 0);
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, power);
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, power);
-	  HAL_Delay(delay);
-	  //pahse3
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0);
-	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, power);
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, power);
-	  HAL_Delay(delay);
-	  //phase4
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
-	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, power);
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, power);
-	  HAL_Delay(delay);
-	  //phase5
-	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, 0);
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, power);
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, power);
-	  HAL_Delay(delay);
-	  //phase6
-	  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0);
-	  __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, power);
-	  __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, power);
-	  HAL_Delay(delay);
-	  counter++;
-	  if(counter>PWM_LIMIT)counter=0;
+	  counter_U++;
+	  if (counter_U>NUM_ANGLE_SPLIT-1)counter_U=0;
+	  counter_V++;
+	  if (counter_V>NUM_ANGLE_SPLIT-1)counter_V=0;
+	  counter_W++;
+	  if (counter_W>NUM_ANGLE_SPLIT-1)counter_W=0;
 
     /* USER CODE END WHILE */
 
